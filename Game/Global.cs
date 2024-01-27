ac3d0c9328ca606bbbaa9297e3bf080c0959d368
Tilde <pxn.tilde@proton.me>
2024-01-27 12:46:57 -0800

    Global class for tracking daters

---
+++ Game/Global.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Godot;
+namespace ggj2024;
+
+/// <summary>
+/// Game jammy way to keep track of daters. Not even a singleton. Wow. Some people says it's an anti-pattern anyway
+/// </summary>
+public static class Global
+{
+    public static Single MaxDistance { get; set; }
+    public static Single MaxHeight { get; set; }
+    public static Single MaxTime { get; set; }
+}
