f69d9230d901ab9c40283f7da93b9adfabc4e5f6
AdamKauth <adamkauth@gmail.com>
2024-01-27 12:06:22 -0800

    Adam scene. Ground collider follows player

---
+++ Game/Adam/Ground.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class Ground : StaticBody2D
+{
+    [Export]
+    public RigidBody2D shootie;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+        Position = new Vector2(shootie.Position.X, Position.Y);
+	}
+}
