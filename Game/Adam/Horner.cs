989582c485655f63d0d221d8b3f1d99132d6efb8
AdamKauth <adamkauth@gmail.com>
2024-01-27 17:24:42 -0800

    Horn.wav

---
+++ Game/Adam/Horner.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class Horner : Area2D
+{
+    private AudioStreamPlayer hornPlayer;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+        hornPlayer = GetNode<AudioStreamPlayer>("HornPlayer");
+		BodyEntered += onBodyEntered;
+	}
+
+	private void onBodyEntered(Node2D body)
+	{
+        GD.Print("horn: " + hornPlayer);
+        hornPlayer.Play();
+	}
+}
