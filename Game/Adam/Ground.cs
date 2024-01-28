336842df2fc2b39b26f6a863ce6041c995b20a54
AdamKauth <adamkauth@gmail.com>
2024-01-28 02:20:10 -0800

    Show height above screen when off screen

--- Game/Adam/Ground.cs
+++ Game/Adam/Ground.cs
@@ -5,14 +5,22 @@ public partial class Ground : StaticBody2D
 {
     [Export]
     public RigidBody2D shootie;
+    private Label heightLabel;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+        heightLabel = GetNode<Label>("Label");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
         Position = new Vector2(shootie.Position.X, Position.Y);
+        if (shootie.heightAbove > 0) {
+            heightLabel.Text = shootie.heightAbove + " ft";
+            heightLabel.Show();
+        } else if (heightLabel.Visible) {
+            heightLabel.Hide();
+        } 
 	}
 }
