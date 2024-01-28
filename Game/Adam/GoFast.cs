b3b0e00c663bab15982baf3b920600877d4074bb
AdamKauth <adamkauth@gmail.com>
2024-01-27 22:00:22 -0800

    Horizontal boost obstacle

---
+++ Game/Adam/GoFast.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class GoFast : Area2D
+{
+    private AudioStreamPlayer skrrtPlayer;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+        skrrtPlayer = GetNode<AudioStreamPlayer>("SkrrtPlayer");
+		BodyEntered += onBodyEntered;
+	}
+
+	private void onBodyEntered(Node2D body)
+	{
+        skrrtPlayer.Play();
+        ((RigidBody2D)body).ApplyCentralImpulse(new Vector2(500f, 0f) * 10f);
+	}
+}
